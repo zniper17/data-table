81c868e875f88c75a7eb166c251ed74d28cf7513
shaham-edison <shaham@teachedison.com>
2023-07-16 01:14:32 +0300

    removed dummy data, fetched data using server side components

--- src/app/examples/page.tsx
+++ src/app/examples/page.tsx
@@ -1,41 +1,28 @@
+import { getPostPage } from "@/api/axisos";
 import DataTable from "@/components/DataTable";
 import React from "react";
-function Home() {
-  const headers = [
-    "Timestamp",
-    "Purchase Id",
-    "Mail",
-    "Name",
-    "Source",
-    "Status",
-    "Select",
-  ];
+async function Home({ searchParams }: any) {
+  const headers = ["Timestamp", "Purchase Id", "Mail", "Name"];
+  const totalData = 100;
+  const perPageData = 10;
+  const totalPages = totalData / perPageData;
+  let currentPage = 1;
+  if (Number(searchParams.page_num) >= 1) {
+    currentPage = Number(searchParams.page_num);
+  }
 
-  const rows = [
-    {
-      timestamp: "2023-07-15",
-      purchaseId: "123",
-      mail: "example@example.com",
-      name: "John Doe",
-      source: "Online",
-      status: "Completed",
-      select: "Yes",
-    },
-    {
-      timestamp: "2023-07-14",
-      purchaseId: "456",
-      mail: "test@test.com",
-      name: "Jane Smith",
-      source: "In-store",
-      status: "Pending",
-      select: "No",
-    },
-    // Add more rows as needed
-  ];
+  const data = await getPostPage(searchParams.page_num);
 
   return (
     <React.Fragment>
-      <DataTable sortable caption="Bookings" headers={headers} rows={rows} />
+      <DataTable
+        sortable
+        caption="Bookings"
+        headers={headers}
+        rows={data}
+        currentPage={currentPage}
+        totalPages={totalPages}
+      />
     </React.Fragment>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,9 @@
 import DataTable from "@/components/DataTable";
 import React from "react";
 function Home() {
-  const headers = [
-    "Timestamp",
-    "Purchase Id",
-    "Mail",
-    "Name",
-    "Source",
-    "Status",
-    "Select",
-  ];
-
-  const rows = [
-    {
-      timestamp: "2023-07-15",
-      purchaseId: "123",
-      mail: "example@example.com",
-      name: "John Doe",
-      source: "Online",
-      status: "Completed",
-      select: "Yes",
-    },
-    {
-      timestamp: "2023-07-14",
-      purchaseId: "456",
-      mail: "test@test.com",
-      name: "Jane Smith",
-      source: "In-store",
-      status: "Pending",
-      select: "No",
-    },
-    // Add more rows as needed
-  ];
-
   return (
     <React.Fragment>
-      <DataTable sortable caption="Bookings" headers={headers} rows={rows} />
+      <h1>Welcome</h1>
     </React.Fragment>
   );
 }
